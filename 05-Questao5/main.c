16c3999dd01a75d6a0932ce58e062a8481c1f869
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2022-10-07 20:41:32 -0300

    Question 5

---
+++ 05-Questao5/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    for(int i=0; i<128; i++){
+        printf("codigo decimal %d| codigo hexa %x| caractere %c\n",i,i,i);
+    }
+
+    return 0;
+}
